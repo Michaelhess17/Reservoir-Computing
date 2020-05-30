60ed5bd747ba73dc4576af5701661909a36027cd
Michael Lee <michael@Michaels-MBP-8.socal.rr.com>
2020-05-30 16:18:16 -0700

    modified EulerMoSComp.m to take multiple integration steps by altering interpolation length. 2000for0.1... is one of the graphs from letting it run.

--- Matlab/EulerMoSComp.m
+++ Matlab/EulerMoSComp.m
@@ -1,68 +1,85 @@
 
-%TESTS 
+%% TESTS
 
-%d = dde23Comp(Inputsequence,1,1,2,1,7,0.2);
-% d = ddeVEuler(Inputsequence,0.05);
+    %% TEST Sampling Different Points
 
-% subplot(1,2,1);
-% [d,t] = dde23Comp(2,2,9.65,1,300, 1.3);
-% subplot(1,2,2);
-% d = MGEulerComp(2, 2, 9.6, 1, 300, .0000500, 1.3);
- 
-% count = 1;
-% input = [0.9,0.1,0.001,0.0001,0.00001];         % At this moment, seems like 0.00001 is the lowest magnitude allowed
-% for n = input
-%     subplot(size(input,2), 1, count);
-%     MGEulerComp(2, 2, 9.6, 1, 300, n, 1.3)
-%     count = count + 1
-% end
+    % count = 1;
+    % input = [0.9,0.1,0.001,0.0001,0.00001];         % At this moment, seems like 0.00001 is the lowest magnitude allowed
+    % for n = input
+    %     subplot(size(input,2), 1, count);
+    %     MGEulerComp(2, 2, 9.6, 1, 300, n, 1.3)
+    %     count = count + 1
+    % end
+    
+    %% TEST MGEulercomp
 
-% d,e] = MGEulerComp(2, 2, 9.6, 1, 300, 0.001, 1.3)
+    % d,e = MGEulerComp(2, 2, 9.6, 1, 300, 0.001, 1.3)
 
-% END OF TESTS
+    %% TEST dde23comp
 
- d = ddeVEuler(0.400750375187594,2,false);
+    % d = dde23Comp(Inputsequence,1,1,2,1,7,0.2);
+
+    %% TEST Comparison Functions
+
+    % d = ddeVEuler(0.01,2,false);
+
+    d = ddeVEuler(0.01,2,true);
+
+    % Value not Working ->  eulert start point changed to 0
+    % d = ddeVEuler(0.400750375187594,2,false);     
+
+    % subplot(1,2,1);
+    % [d,t] = dde23Comp(2,2,9.65,1,300, 1.3);
+    % subplot(1,2,2);
+    % d = MGEulerComp(2, 2, 9.6, 1, 300, .0000500, 1.3);
 
 
+%% COMPARISON LOOPS
+    %% Calculate and graph 0.000001 -> 0.1
 
-% % 0.000001 -> 0.1
-% testRange = linspace(0.000001,0.1,200);
-% counter = 1;
+    % testRange = linspace(0.000001,0.1,200);
+    % counter = 1;
+    % 
+    % for n = testRange
+    %     spError(counter) = ddeVEuler(n,2,false);
+    %     counter = counter +1;
+    % end
+    % 
+    % plot(testRange(:), spError);
+    % xlabel("Integration Step");
+    % ylabel("squared error");
+    % title("error for different euler integration steps")
+
+    %% 2000 points between 0.01 -> 1
+
+%     testRange = linspace(0.01,1,2000);
+%     counter = 1;
 % 
-% for n = testRange
-%     spError(counter) = ddeVEuler(n,2,false);
-%     counter = counter +1;
-% end
+%     for n = testRange
+%         spError(counter) = ddeVEuler(n,2,false);
+%         counter = counter +1;
+%     end
 % 
-% plot(testRange(:), spError);
-% xlabel("Integration Step");
-% ylabel("squared error");
-% title("error for different euler integration steps")
+%     plot(testRange(:), spError);
+%     xlabel("Integration Step");
+%     ylabel("squared error");
+%     title("error for different euler integration steps")
 
-% % 0.1 -> ~5
-% testRange = linspace(0.01,1,2000);
-% counter = 1;
-% 
-% for n = testRange
-%     spError(counter) = ddeVEuler(n,2,false);
-%     counter = counter +1;
-% end
-% 
-% plot(testRange(:), spError);
-% xlabel("Integration Step");
-% ylabel("squared error");
-% title("error for different euler integration steps")
+    %% 
+
+
+%% FUNCTIONS
 
 function [eulert, M_x] = MGEulerComp( tau, eta, p, gamma, iterations, theta, initial)
     % Calculates output of MG using euler 
     
     % output:
     % M_x: Matrix of reservoir history of size (# tau, Nodes)
-    
     N = floor(tau ./ theta);            % Determine how many appoximate nodes we can fit given some theta
     M_x = ones(1,N) .* initial;         % Set first row(initial conditions) to some value
 
-    for i = 2:iterations+1
+    %Calculates for iterations after the initial conditions
+    for i = 2:iterations + 1
         for j = 0       %Used to evaluate MG for the first value in a new column
             vn_0 = M_x(i-1,end) + (- gamma .* M_x(i-1,end) + (eta .* M_x(i-1,end)) ...
                 ./ (1 + (M_x(i-1,end)).^ p)) .* theta;
@@ -80,10 +97,12 @@ function [eulert, M_x] = MGEulerComp( tau, eta, p, gamma, iterations, theta, ini
     M_x = reshape(M_x.',1,[]);
     
     % Get the x coordinates that match with each value of M_x
-    total = tau .* iterations;
-    eulert = [theta: total ./ (N .* iterations): total];
+    total = tau .* (iterations);
+    eulert = [0: total ./ (N .* (iterations)): total];          % Changed the start to 0 from theta
+    M_x = [initial M_x];            % Adds in Initial condition into history
     
-     plot(eulert, M_x);
+    
+%     plot(eulert, M_x); Sanity Test Plot
    
 end
 
@@ -142,19 +161,20 @@ function diff = ddeVEuler(theta, tau, plotOn)
     [eulert, eulerSol] = MGEulerComp(tau, eta, p, gamma, iterations, theta, initial);
     [ddeSolt,ddeSoly] = dde23Comp( tau, eta, p, gamma, iterations, initial);
     
-    % Find the values at certain x values of Euler solution to compare 
+    % Find the values at certain x values (matching with dde solutions) of Euler solution to compare 
     EulerPts = interp1(eulert, eulerSol, ddeSolt);
     
     % Compare results and calculate total sq. error
-    
     error = EulerPts - ddeSoly;
-    error = error(2:end);           % Cut off the first value because euler begins recording at first calculation
-    error = error(1:end-1);         % Snip off the end to account for other discontinuities
+    
+    % Snip off parts 
+%     error = error(2:end);           % Cut off the first value because euler begins recording at first calculation
+%     error = error(1:end-1);         % Snip off the end to account for other discontinuities
     SqError = error .^ 2;
     size(error);
     diff = mean(mean(SqError,1),2);
-    
 
+    
     % Plot it all out
     if plotOn == true
         figure1 = figure;
@@ -167,10 +187,19 @@ function diff = ddeVEuler(theta, tau, plotOn)
         title("DDE23 vs. Euler")
         hold off;
         legend("DDE23", "Euler");
+        ylabel("x(t)");
+        xlabel("t");
+        a = gca;
+        a.Position(3) = 0.60
+        annotation('textbox',[ 0.75, 0.85, 0.1, 0.1], 'String', ["eta=" + num2str(eta)  "p =" + num2str(p)+ ",gamma=" + num2str(gamma)...
+             "Iterations of tau = " + num2str(iterations)  "Initialvalue = " + num2str(initial)...
+             "Integration step (theta) =" + num2str(theta)  "tau=" + num2str(tau)]);
 
         subplot(2,1,2)
-        plot(ddeSolt(2:end), SqError);
-        title(["Mean Squared Error = ",num2str(diff)])
+        plot(ddeSolt, SqError);
+        title(["Mean Squared Error = ",num2str(diff)]);
+        xlabel("t")
+        ylabel("squared error")
 
 
         saveas(figure1,'EulervDDE23.png')
